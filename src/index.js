1db42b82c0de30201504d46f9fdf92192991fd77
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2023-11-06 18:13:02 +0100

    Add GitHub workflow trigger on lifecycle events

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -16,5 +16,46 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    const triggerGithubWorkflow = async (event) => {
+      try {
+        const response = await fetch(
+          "https://api.github.com/repos/Pierpaolo01/vidicrowd-website/dispatches",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/vnd.github.everest-preview+json",
+              Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              event_type: "strapi",
+            }),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`GitHub dispatch failed: ${response.status}`);
+        }
+
+        console.log(
+          `GitHub Actions workflow for ${event.action} dispatch triggered successfully.`
+        );
+      } catch (error) {
+        console.error("Error triggering GitHub Actions workflow:", error);
+      }
+    };
+
+    strapi.db.lifecycles.subscribe({
+      afterCreate(event) {
+        triggerGithubWorkflow(event);
+      },
+      afterUpdate(event) {
+        triggerGithubWorkflow(event);
+      },
+      afterDelete(event) {
+        triggerGithubWorkflow(event);
+      },
+    });
+  },
 };
