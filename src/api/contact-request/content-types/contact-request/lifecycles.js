2fbac1feacb0e35427f41fec83b4d8a6991d3258
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2023-11-19 13:35:06 +0100

    Implement email service

---
+++ src/api/contact-request/content-types/contact-request/lifecycles.js
@@ -0,0 +1,14 @@
+module.exports = {
+  async afterCreate({ result }) {
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "pierpaolo.pascarella@gmail.com", //TODO change to jacco's email
+        from: "jacco.taal@pascarella-development.com",
+        subject: "New contact request",
+        text: `You recived a form submission from: ${result.name}, (${result.email}). With the following message: ${result.description}}`,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
